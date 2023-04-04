a1afbfa79f5c2c56cb99e4603aa1f0ae1d55020e
takatorige yusuke <129149972+takatorigeyusuke@users.noreply.github.com>
2023-04-04 18:58:55 +0900

    レスポンシブデザインがわかりません。

---
+++ STUDY/index.php
@@ -0,0 +1,29 @@
+<?php
+echo 1+1;
+echo '<br>';
+echo 3-1;
+echo '<br>';
+echo 3*3;
+echo '<br>';
+echo 6/2;
+echo '<br>';
+echo 7%2;
+echo '<br>';
+
+$x = 1;
+$x++;
+echo $x;
+echo '<br>';
+$x--;
+echo $x;
+echo '<br>';
+$x += 5;
+echo $x;
+echo '<br>';
+$x -= 2;
+echo $x;
+echo '<br>';
+$x *= 5;
+echo $x;
+
+?>
