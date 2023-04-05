bbcdb608ea8ba3c519960251f664a538f473ed3f
takatorige yusuke <129149972+takatorigeyusuke@users.noreply.github.com>
2023-04-05 22:28:34 +0900

    課題提出

--- STUDY/index.php
+++ STUDY/index.php
@@ -1,29 +1,7 @@
 <?php
-echo 1+1;
-echo '<br>';
-echo 3-1;
-echo '<br>';
-echo 3*3;
-echo '<br>';
-echo 6/2;
-echo '<br>';
-echo 7%2;
-echo '<br>';
-
-$x = 1;
-$x++;
-echo $x;
-echo '<br>';
-$x--;
-echo $x;
-echo '<br>';
-$x += 5;
-echo $x;
-echo '<br>';
-$x -= 2;
-echo $x;
-echo '<br>';
-$x *= 5;
-echo $x;
-
+$num = 0;
+for($i = 0;$i <= 100;$i++) {
+    echo $i;
+    echo '<br>';
+}
 ?>
